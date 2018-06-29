319488cfe7b1069e9d0d4a1ff72b87e748d2db16
Ben Schoepke <benjasc@microsoft.com>
2018-06-29 10:52:10 -0700

    Add ability to open all tabs in Edge.
    Not publishing because it requires broader "tabs" permission.

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
-const contextMenuId = 'openLinkInMicrosoftEdge';
+const linkContextMenuId = 'openLinkInMicrosoftEdge';
+const frameContextMenuId = 'openTabsInMicrosoftEdge';
 
 function openInEdge(url) {
   chrome.tabs.update({
@@ -7,8 +8,17 @@ function openInEdge(url) {
 }
 
 function onOpenInEdgeItemClicked(info, tab) {
-  if (info.linkUrl && (info.menuItemId === contextMenuId)) {
+  if (info.linkUrl && (info.menuItemId === linkContextMenuId)) {
     openInEdge(info.linkUrl);
+  } else if (info.menuItemId === frameContextMenuId) {
+    chrome.tabs.query({
+      currentWindow: true,
+    }, tabs => {
+      for (let i = 0; i < tabs.length; i++) {
+        const tabUrl = tabs[i].url;
+        setTimeout(() => openInEdge(tabUrl), (i + 1) * 10);
+      }
+    });
   }
 }
 
@@ -16,9 +26,15 @@ chrome.contextMenus.onClicked.addListener(onOpenInEdgeItemClicked);
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
-    'id': contextMenuId,
+    'id': linkContextMenuId,
     'title': 'Open link in Microsoft Edge',
     'contexts': ['link']
+  });
+
+  chrome.contextMenus.create({
+    'id': frameContextMenuId,
+    'title': 'Open all tabs in Microsoft Edge',
+    'contexts': ['browser_action']
   });
 });
 
