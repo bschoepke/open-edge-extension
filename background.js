7886ed84026c5739a86f23ebf7cd42b384deb9b7
Ben Schoepke <benjasc@microsoft.com>
2018-06-29 09:06:24 -0700

    Add "open link microsoft edge"

--- background.js
+++ background.js
@@ -1,5 +1,27 @@
-chrome.browserAction.onClicked.addListener(tab => {
+const contextMenuId = 'openLinkInMicrosoftEdge';
+
+function openInEdge(url) {
   chrome.tabs.update({
-    url: `microsoft-edge:${tab.url}`
+    url: `microsoft-edge:${url}`
   });
+}
+
+function onOpenInEdgeItemClicked(info, tab) {
+  if (info.linkUrl && (info.menuItemId === contextMenuId)) {
+    openInEdge(info.linkUrl);
+  }
+}
+
+chrome.contextMenus.onClicked.addListener(onOpenInEdgeItemClicked);
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    'id': contextMenuId,
+    'title': 'Open link in Microsoft Edge',
+    'contexts': ['link']
+  });
+});
+
+chrome.browserAction.onClicked.addListener(tab => {
+  openInEdge(tab.url);
 });
